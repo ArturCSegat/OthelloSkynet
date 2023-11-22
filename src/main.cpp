f4481cd32e0cd42ad7b39b2df5cce9c894c6daff
ArturCSegat <arturcamerasegat@gmail.com>
2023-11-22 00:47:55 -0300

    added botched solution with polling to show players move + added win and draw detection

--- src/main.cpp
+++ src/main.cpp
@@ -33,11 +33,58 @@ int main() {
             });
 
     CROW_ROUTE(app, "/play/<int>/<int>")([&g](int i, int j){
-            g.play(Coord{i, j});
-            g.play(g.players[g.curr_idx]->choseSquare(g));
-
+            int over = g.play(Coord{i, j});
+            
             inja::Environment env {"templates/"};
             inja::json data;
+
+
+            data["board"] = g.board.board;
+            data["empty"] = g.board.empty_square_marker;
+            data["player1"] = g.players[0]->piece;
+            data["player2"] = g.players[1]->piece;
+            data["p1_count"] = g.players[0]->piece_count;
+            data["p2_count"] = g.players[1]->piece_count;
+            data["turn"] = g.players[g.curr_idx]->piece;
+            auto page = env.render_file("game-info.html", data);
+
+            // playing after rendering the page will ensure the player
+            // does not receive the up to date state of the board, but the changes
+            // their play made, you should then call "/game" to get the updated with CPU move
+            g.play(g.players[g.curr_idx]->choseSquare(g));
+            return page;
+            });
+
+    CROW_ROUTE(app, "/game")([&g](){
+            inja::Environment env {"templates/"};
+            inja::json data;
+
+            if (g.running == false) {
+                data["player1"] = g.players[0]->piece;
+                data["player2"] = g.players[1]->piece;
+                data["p1_count"] = g.players[0]->piece_count;
+                data["p2_count"] = g.players[1]->piece_count;
+                
+                int winner_idx = g.players[1]->piece_count > g.players[0]->piece_count;
+                int reverse = g.players[0]->piece_count > g.players[1]->piece_count;
+
+                std::string end_text;
+
+                if (winner_idx == reverse) {
+                    end_text = "\n\n The Grame Drawed \n\n";
+                } else {
+                    std::ostringstream oss;
+                    oss << "\n\n The Winner of game " << " is " << g.players[winner_idx]->piece << "\n\n";
+                    end_text = oss.str();
+                }
+
+                data["end-text"] = end_text;
+                data["winner"] = g.players[winner_idx]->piece;
+                auto page = env.render_file("end-game.html", data);
+                return page;
+            }
+
+
             data["board"] = g.board.board;
             data["empty"] = g.board.empty_square_marker;
             data["player1"] = g.players[0]->piece;
@@ -49,6 +96,7 @@ int main() {
 
             return page;
             });
+
 
     app.port(5000).multithreaded().run();
 }
